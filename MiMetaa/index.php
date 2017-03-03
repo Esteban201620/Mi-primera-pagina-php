198aedb618f9f1d00ad40076ca6fd0f7d3fcb41f
Estudiantes <Estudiantes@LABING50207.udistrital.edu.co>
2017-03-03 18:02:11 -0500

    Mi primera página

---
+++ MiMetaa/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<b>En 15 años voy a tener mi propia empresa de creacion de software, se que soy medio tortica para programar, pero creo en la disciplina :'v</b>";
+        // put your code here
+        ?>
+    </body>
+</html>
